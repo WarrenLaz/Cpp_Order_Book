9ff1459dfd806e56ccc2dec107a843be6c9a637a
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2025-01-14 18:05:56 -0500

    added some stuff

--- main.cpp
+++ main.cpp
@@ -16,7 +16,8 @@ struct Order{
 
 class OrderBook{
     private:
-        unordered_map<string, Order> orders;
+        unordered_map<string, Order> orders_Asks;
+        unordered_map<string, Order> orders_Bids;
     protected:
         int* Market_(Order ord_){
             return nullptr;
@@ -32,7 +33,12 @@ class OrderBook{
 
         bool AddOrder(Order order_, string name){
             try{
-                orders[name] = order_;
+                if(order_.type == SELL){
+                    orders_Asks[name] = order_;
+                }
+                if(order_.type == BUY){
+                    orders_Bids[name] = order_;
+                }
             } catch (exception){
                 return false;
             }
@@ -48,16 +54,23 @@ class OrderBook{
         }
 
         string Status(string name){
-            if(orders.find(name) != orders.end()){
+            if(orders_Asks.find(name) != orders_Asks.end() || orders_Bids.find(name) != orders_Bids.end()){
+                Order orders = orders_Bids.find(name) != orders_Asks.end() ? orders_Asks[name] : orders_Bids[name];
                 string order;
                 string type;
-                switch(orders[name].ordertype){case MARKET: order="MARKET";break;case LIMIT: order="LIMIT";break;case STOP: order="STOP";}
-                type = orders[name].type == SELL ? "SELL" : "BUY";
-                return name+" ["+type+" "+order+" "+to_string(orders[name].status) + " "+to_string(orders[name].price)+" "+to_string(orders[name].volume) + "]";
+                switch(orders.ordertype){case MARKET: order="MARKET";break;case LIMIT: order="LIMIT";break;case STOP: order="STOP";}
+                type = orders.type == SELL ? "SELL" : "BUY";
+                return name+" ["+type+" "+order+" "+to_string(orders.status) + " "+to_string(orders.price)+" "+to_string(orders.volume) + "]";
             }
             return "Order Not Found";
         }
 
+        string printBook(){
+            string book = "ASKS\tBIDS\n"
+            for(const auto& pair : orders) {
+                book += 
+            }
+        }
         int* Sell(string name){
             
             return nullptr;
@@ -75,6 +88,7 @@ int main(){
     OrderBook orderbook;
     Order myOrder = {BUY, MARKET, 10, 20.00, 0};
     orderbook.AddOrder(myOrder, "Warren Lazarraga");
+    
     cout << orderbook.Status("Warren Lazarraga");
     return 0;
 }
